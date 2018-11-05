3a6e5d23601f7d3559ec838ed6d21da6c7a92b60
oranleo <44780771+oranleo@users.noreply.github.com>
2018-11-05 16:40:29 +0000

    Initial commit

---
+++ application/views/templates/leftpanel.php
@@ -0,0 +1,85 @@
+   <!-- Left Panel -->
+
+    <aside id="left-panel" class="left-panel">
+        <nav class="navbar navbar-expand-sm navbar-default">
+
+            <div class="navbar-header">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-menu" aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation">
+                    <i class="fa fa-bars"></i>
+                </button>
+                <a class="navbar-brand" href="./"><img src="templates/images/logo.png" alt="Logo"></a>
+                <a class="navbar-brand hidden" href="./"><img src="templates/images/logo2.png" alt="Logo"></a>
+            </div>
+
+            <div id="main-menu" class="main-menu collapse navbar-collapse">
+                <ul class="nav navbar-nav">
+                    <li class="active">
+                        <a href="index.html"> <i class="menu-icon fa fa-dashboard"></i>Dashboard </a>
+                    </li>
+                    <h3 class="menu-title">Admin</h3><!-- /.menu-title -->
+                    <li class="menu-item-has-children dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-laptop"></i>Admin Controls</a>
+                        <ul class="sub-menu children dropdown-menu">
+                            <li><i class="fa fa-puzzle-piece"></i><a href="ui-buttons.html">Access Managament</a></li>
+                            <li><i class="fa fa-id-badge"></i><a href="ui-badges.html">Add New Admin</a></li>
+                            <li><i class="fa fa-id-badge"></i><a href="ui-badges.html">Edit Admin</a></li>
+                            <li><i class="fa fa-bars"></i><a href="ui-tabs.html">Add New Hoca</a></li>
+                            <li><i class="fa fa-bars"></i><a href="ui-tabs.html">Edit Hoca</a></li>
+                            <li><i class="fa fa-share-square-o"></i><a href="ui-social-buttons.html">Add New Talebe</a></li>
+                            <li><i class="fa fa-share-square-o"></i><a href="ui-social-buttons.html">Edit Talebe</a></li>
+                            <li><i class="fa fa-id-card-o"></i><a href="ui-cards.html">Add New Lesson</a></li>
+                            <li><i class="fa fa-id-card-o"></i><a href="ui-cards.html">Edit Lesson</a></li>
+                            <li><i class="fa fa-exclamation-triangle"></i><a href="ui-alerts.html">Alerts</a></li>
+                            
+                        </ul>
+                    </li>
+                    <li class="menu-item-has-children dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-table"></i>Charts</a>
+                        <ul class="sub-menu children dropdown-menu">
+                            <li><i class="fa fa-table"></i><a href="tables-basic.html">Daily Chart</a></li>
+                            <li><i class="fa fa-table"></i><a href="tables-data.html">Weekly Chart</a></li>
+                            <li><i class="fa fa-table"></i><a href="tables-data.html">Monthly Chart</a></li>
+                            <li><i class="fa fa-table"></i><a href="tables-data.html">Querter Chart</a></li>
+                            <li><i class="fa fa-table"></i><a href="tables-data.html">Annual Chart</a></li>
+                        </ul>
+                    </li>
+
+                    <h3 class="menu-title">Hoca</h3><!-- /.menu-title -->
+
+                    <li class="menu-item-has-children dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-tasks"></i>Hoca Managament</a>
+                        <ul class="sub-menu children dropdown-menu">
+                            <li><i class="fa fa-share-square-o"></i><a href="ui-social-buttons.html">Add New Talebe</a></li>
+                            <li><i class="fa fa-share-square-o"></i><a href="ui-social-buttons.html">Edit Talebe</a></li>
+                            <li><i class="fa fa-id-card-o"></i><a href="ui-cards.html">Add New Lesson</a></li>
+                            <li><i class="fa fa-id-card-o"></i><a href="ui-cards.html">Edit Lesson</a></li>
+                            <li><i class="fa fa-id-card-o"></i><a href="ui-cards.html">Edit Profile</a></li>
+                        </ul>
+                    </li>
+                    <li class="menu-item-has-children dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-bar-chart"></i>Charts</a>
+                        <ul class="sub-menu children dropdown-menu">
+                            <li><i class="fa fa-table"></i><a href="tables-basic.html">Daily Chart</a></li>
+                            <li><i class="fa fa-table"></i><a href="tables-data.html">Weekly Chart</a></li>
+                            <li><i class="fa fa-table"></i><a href="tables-data.html">Monthly Chart</a></li>
+                            <li><i class="fa fa-table"></i><a href="tables-data.html">Querter Chart</a></li>
+                            <li><i class="fa fa-table"></i><a href="tables-data.html">Annual Chart</a></li>
+                        </ul>
+                    </li>
+
+                    
+                    <h3 class="menu-title">Talebe</h3><!-- /.menu-title -->
+                    <li class="menu-item-has-children dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> <i class="menu-icon fa fa-glass"></i>Profile</a>
+                        <ul class="sub-menu children dropdown-menu">
+                            <li><i class="fa fa-id-card-o"></i><a href="ui-cards.html">Login</a></li>
+                            <li><i class="fa fa-id-card-o"></i><a href="ui-cards.html">Register</a></li>
+                            <li><i class="fa fa-id-card-o"></i><a href="ui-cards.html">Edit Profile</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div><!-- /.navbar-collapse -->
+        </nav>
+    </aside><!-- /#left-panel -->
+
+    <!-- Left Panel -->
